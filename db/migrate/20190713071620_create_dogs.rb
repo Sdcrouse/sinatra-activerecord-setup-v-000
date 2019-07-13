14b8af59b7a694b23f0c57a5acea22fe4c8ad049
Steven Crouse <sdcrouse91@gmail.com>
2019-07-13 08:00:24 +0000

    Automatically backed up by Learn

--- db/migrate/20190713071620_create_dogs.rb
+++ db/migrate/20190713071620_create_dogs.rb
@@ -1,14 +1,18 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
-  def up
-    create_table :dogs do |t|
-      t.string :name
-      t.string :breed
-    end
-    # add_column :dogs, :name, :text
-    # add_column :dogs, :breed, :text
-  end 
+  # def up
+    # create_table :dogs do |t|
+      # t.string :name
+      # t.string :breed
+    # end
+      # add_column :dogs, :name, :text
+      # add_column :dogs, :breed, :text
+  # end 
   
-  def down
-    drop_table :dogs
+  # def down
+    # drop_table :dogs
+  # end
+  
+  def change
+    
   end
 end
